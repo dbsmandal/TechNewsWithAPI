3ab47ee080823061af153a6109ce94270cc98328
dbsmandal <dipakmandaljio420@gmail.com>
2022-09-17 23:16:32 +0530

    Calling API and Fetch Data in console

--- src/Stories.jsx
+++ src/Stories.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const Stories = () => {
+    let API ="http://hn.algolia.com/api/v1/search?query=foo&tags=story"
+
+    const fetchApiData=async(url)=>{
+        try {
+            const res=await fetch(url);
+            const data=await res.json();
+            console.log(data)
+
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    useEffect(()=>{
+        fetchApiData(API)
+    },[]);
+
   return (
-    <div>
-      <h4>Wlcome Stories</h4>
-    </div>
+    <>
+          <h4>Wlcome Stories</h4>
+
+    </>
   )
 }
 
