83909811957031da619aebaa6abd9bb213f177c3
dbsmandal <dipakmandaljio420@gmail.com>
2022-09-19 12:29:04 +0530

    The project is Complete With Responsive

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,11 +1,55 @@
-import React from 'react'
+import React from "react";
+import "./footer.css"
+import { FaFacebook,FaGithub,FaLinkedin} from "react-icons/fa";
 
 const Footer = () => {
-  return (
-    <div className='fotter-heading'>
-      <h1 >This Website is my 3rd React Project</h1 >
-    </div>
-  )
-}
+  return (<>
+   <footer>
+   <div className="container grid grid-three-column">
 
+<div className="footer-about">
+  <h3>Dbsmandal</h3>
+  <p>Hi, I am Dipak Mandal And it is my 3rd Project in React, In this Project Some features are useContext,useEffect, useReducer,Fetch API etc</p>
+</div>
+
+
+<div className="footer-social">
+  <h3>Check My Social Profile</h3>
+  <div className="footer-social--icons">
+    <div>
+    <a
+        href="https://www.linkedin.com/feed/?trk=nav_back_to_linkedin"
+        target="_blank">
+        <FaLinkedin className="icons" />
+      </a>
+  
+    </div>
+    <div>
+    <a
+     href="https://www.facebook.com/"
+     target="_blank">
+      <FaFacebook className="icons" />
+      </a>
+    </div>
+    <div>
+      <a
+        href="https://github.com/dbsmandal"
+        target="_blank">
+        <FaGithub className="icons" />
+      </a>
+    </div>
+  </div>
+</div>
+
+
+<div className="footer-contact">
+  <h3>Call Me</h3>
+  <h3>+91 9734078407</h3>
+</div>
+</div>
+
+   </footer>
+  
+  </>);
+};
 export default Footer;
